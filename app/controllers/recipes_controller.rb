f1c58463d5a0cd1c741819e5d12f235c77dabad3
channedesign <channebertrand@gmail.com>
2024-10-19 11:07:51 +0200

    feat(recipe_search): remove not needed code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,4 @@
 class RecipesController < ApplicationController
   def index
-    # @pagy, @recipes = pagy(RecipeSearchService.new(params[:ingredients]).call)
-
-    if params[:ingredients]
-      @ingredients = params[:ingredients].split(",").map(&:strip).map(&:downcase)
-      @pagy, @recipes = pagy(Recipe.with_at_least_one_inputed_ingredients(@ingredients))
-    else
-      @pagy, @recipes = pagy(Recipe.all)
-    end
-  end
+    @pagy, @recipes = pagy(RecipeSearchService.new(params[:ingredients]).call)
 end
