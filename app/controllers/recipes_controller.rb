af7355ca62db274eb550158c57fb43400e39a775
channedesign <channebertrand@gmail.com>
2024-10-20 16:05:20 +0200

    feat(recipe): add more scopes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,30 @@
 class RecipesController < ApplicationController
   def index
-    @pagy, @recipes = pagy(RecipeSearchService.new(params[:ingredients]).call)
+    begin 
+      if params[:ingredients].present?
+        @ingredients = params[:ingredients].split(",").map(&:strip).map(&:downcase)
+
+        recipe_ids = Recipe.with_all_inputed_ingredients(@ingredients).pluck(:id) # can't use .includes(:ingredients) because of the use of GROUP BY with aggregate functions in ActiveRecord.
+        recipes = Recipe.includes(:ingredients).where(id: recipe_ids)
+
+        # Apply optional params if present
+        recipes = recipes.with_max_cooking_time(params[:max_cook_time]) if params[:max_cook_time].present?
+        recipes = recipes.with_max_preparation_time(params[:max_prep_time]) if params[:max_prep_time].present?
+        recipes = recipes.with_min_ratings(params[:min_ratings]) if params[:min_ratings].present?
+
+        @pagy, @recipes = pagy(recipes)
+      else
+        @pagy, @recipes = pagy(Recipe.includes(:ingredients).all)
+      end
+    
+    rescue ActiveRecord::RecordNotFound => e
+      flash.now[:alert] = "No recipes found."
+      @pagy, @recipes = pagy(Recipe.includes(:ingredients).none)
+
+    rescue => e
+      flash.now[:alert] = "An error occurred: #{e.message}"
+      @pagy, @recipes = pagy(Recipe.includes(:ingredients).none)
+      Rails.logger.error("Error fetching recipes: #{e.message}")
+    end
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,27 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
+
+  scope :with_all_inputed_ingredients, ->(ingredient_list) {
+    joins(:ingredients)
+      .group("recipes.id")
+      .having(ingredient_list.map { "SUM(CASE WHEN ingredients.name ILIKE ? THEN 1 ELSE 0 END) > 0" }.join(" AND "), *ingredient_list.map { |ingredient| "%#{ingredient}%" })
+  }
+
+  scope :with_max_cooking_time, ->(max_cook_time) {
+    where('cook_time <= ?', max_cook_time)
+  }
+
+  scope :with_max_preparation_time, ->(max_prep_time) {
+    where('prep_time <= ?', max_prep_time)
+  }
+
+  scope :with_min_ratings, ->(min_ratings) {
+    where('ratings >= ?', min_ratings)
+  }
+
+  scope :order_by_ratings, ->() {
+    order(ratings: :desc)
+  }
+
 end
